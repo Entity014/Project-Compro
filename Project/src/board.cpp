c7505aa7454f0772a1b8a554689efd31c3fecf7d
_Entity014 ( Narawit Inyos ) <63171588+Entity014@users.noreply.github.com>
2023-02-17 03:24:37 +0700

    Update board.cpp

--- Project/src/board.cpp
+++ Project/src/board.cpp
@@ -1,6 +1,3 @@
-#include "board.h"
-#include <SFML/Graphics.hpp>
-
 void board(const int flied, const int Size, sf::RectangleShape arrB[])
 {
     int j = 0;
@@ -9,17 +6,23 @@ void board(const int flied, const int Size, sf::RectangleShape arrB[])
         sf::RectangleShape rectangle(sf::Vector2f(Size, Size));
         if (i % 2 == 1 && j % 2 == 0)
         {
-            rectangle.setFillColor(sf::Color::Black);
+            rectangle.setFillColor(sf::Color::Red);
+            rectangle.setPosition(Size*i, 0);
         }
         else if (i % 2 == 0 && j % 2 == 1)
         {
-            rectangle.setFillColor(sf::Color::Black);
+            rectangle.setFillColor(sf::Color::Red);
+            rectangle.setPosition(Size*i, 0);
         }
         else
         {
             rectangle.setFillColor(sf::Color::White);
+            rectangle.setPosition(Size * i, 0);
+        }
+        if (i % flied == fields - 1)
+        {
+            j++;
         }
         *(arrB + i) = rectangle;
-        j++;
     }
 }
