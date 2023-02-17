1630ccea4ca38f6c7d877d9397d73d657360d737
Entity014 <radlove.012@gmail.com>
2023-02-18 05:17:21 +0700

    v.1.2 By Phytes

---
+++ Project/src/include/spawnEntity.cpp
@@ -0,0 +1,50 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <cmath>
+
+void defaultBoardCheck(const int dB[] ,int N, int &count)
+{
+    for(int i = 0; i < N; i++)
+    {
+        if(*(dB + i) != 0)
+        {
+            count++;
+        }
+    }
+    // std::cout << count << std::endl;
+}
+
+void loadTextures(sf::Texture tE[], const std::string pE[], int N)
+{
+    for (int i = 0; i < N; i++)
+    {
+        if(!tE[i].loadFromFile(pE[i]))
+        {
+            std::cout << "Cannot load texture " << pE[i] << std::endl;
+        }
+        tE[i].setSmooth(true);
+    }
+}
+
+void entityConfig(const int dB[], const sf::Texture tE[], sf::Sprite sE[], int N)
+{
+    sf::Texture t;
+    int countE = 0;
+    for(int i = 0; i < N; i++)
+    {
+        if (*(dB + i) < 0)
+        {
+            sE[countE].setTexture(tE[abs(*(dB + i)) - 1]);
+            sE[countE].setScale(sf::Vector2f(0.45f, 0.45f));
+            // std::cout << countE << std::endl;
+            countE++;
+        }
+        else if (*(dB + i) > 0)
+        {
+            sE[countE].setTexture(tE[*(dB + i) + 5]);
+            sE[countE].setScale(sf::Vector2f(0.45f, 0.45f));
+            // std::cout << countE << std::endl;
+            countE++;
+        }
+    }
+}
--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -1,32 +1,70 @@
-#include <iostream>
 #include <vector>
-#include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
-#include "board.h"
+#include <iostream>
+#include <string>
+#include "include/board.cpp"
+#include "include/spawnEntity.cpp"
 
+/* Variable of display */
 const int width = 800, height = 600;
 const int fields = 8;
 const int tileSize = height / fields;
 const int moveRight = width - height;
 
+/* Board */
 sf::RectangleShape boardChess[fields * fields];
 
+/* Entity */
+/* In Future, it must change */
+int defaultBoard[fields * fields] =
+{
+    -1, -2, -3, -4, -5, -3, -2, -1,
+    -6, -6, -6, -6, -6, -6, -6, -6,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    6, 6, 6, 6, 6, 6, 6, 6,
+    1, 2, 3, 5, 4, 3, 2, 1
+};
+int count = 0;
+
+/* Texture */
+std::string pathE[12] = 
+{
+    "asset/texture/b_rook_png_128px.png", "asset/texture/b_knight_png_128px.png", "asset/texture/b_bishop_png_128px.png", "asset/texture/b_queen_png_128px.png", "asset/texture/b_king_png_128px.png", "asset/texture/b_pawn_png_128px.png",
+    "asset/texture/w_rook_png_128px.png", "asset/texture/w_knight_png_128px.png", "asset/texture/w_bishop_png_128px.png", "asset/texture/w_queen_png_128px.png", "asset/texture/w_king_png_128px.png", "asset/texture/w_pawn_png_128px.png"
+};
+std::string pathP[] =
+{
+    "asset/texture/test.png"
+};
+
+sf::Texture textureE[12];
+sf::Texture textureP[1];
 
 int main()
 {
-    sf::Texture t1;
-    t1.loadFromFile("asset/texture/test.png");
-    t1.setSmooth(true);
+    /* Load Textures */
+    loadTextures(textureP, pathP, sizeof(pathP)/sizeof(pathP[0]));
+    loadTextures(textureE, pathE, sizeof(pathE)/sizeof(pathE[0]));
 
-    sf::Sprite s(t1);
-    s.setScale(sf::Vector2f(0.25f, 0.25f));
-    s.setPosition(sf::Vector2f(-175,350));
+    /* Status */
+    sf::Sprite sP1(textureP[0]);
+    sP1.setScale(sf::Vector2f(0.25f, 0.25f));
+    sP1.setPosition(sf::Vector2f(-175,350));
+
+    /* Entity on game */
+    defaultBoardCheck(defaultBoard, sizeof(defaultBoard)/sizeof(defaultBoard[0]), count);
+    sf::Sprite spriteE[count];
+    entityConfig(defaultBoard, textureE, spriteE, fields * fields);
 
     /* Reander window */
     sf::RenderWindow window(sf::VideoMode(width, height), "Chess Mae");
     board(fields, tileSize, moveRight, boardChess);
     sf::RectangleShape Status(sf::Vector2f(moveRight, height));
 
+    /* Window Here */
     while (window.isOpen())
     {
         sf::Event event;
@@ -39,11 +77,12 @@ int main()
         /* Draw */
         window.clear();
         window.draw(Status);
-        window.draw(s);
+        window.draw(sP1);
         for (int i = 0; i < fields * fields; i++)
         {
             window.draw(boardChess[i]);
         }
+        // window.draw(spriteE[0]);
         window.display();
     }
 
