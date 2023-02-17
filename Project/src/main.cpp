39ef2a2ac8449d0495180d910bee1c24031ecd42
_Entity014 ( Narawit Inyos ) <63171588+Entity014@users.noreply.github.com>
2023-02-17 18:23:45 +0700

    Update main.cpp

--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -4,16 +4,28 @@
 #include <SFML/Audio.hpp>
 #include "board.cpp"
 
-const int width = 800, height = 800;
+const int width = 800, height = 600;
 const int fields = 8;
-const int tileSize = width / fields;
+const int tileSize = height / fields;
+const int moveRight = width - height;
 
 sf::RectangleShape boardChess[fields * fields];
 
+
 int main()
 {
+    sf::Texture t1;
+    t1.loadFromFile("/home/entity014/compro/Project/asset/texture/test.png");
+    t1.setSmooth(true);
+
+    sf::Sprite s(t1);
+    s.setScale(sf::Vector2f(0.25f, 0.25f));
+    s.setPosition(sf::Vector2f(-175,350));
+
+    /* Reander window */
     sf::RenderWindow window(sf::VideoMode(width, height), "Chess Mae");
-    board(fields, tileSize, boardChess);
+    board(fields, tileSize, moveRight, boardChess);
+    sf::RectangleShape Status(sf::Vector2f(moveRight, height));
 
     while (window.isOpen())
     {
@@ -24,7 +36,10 @@ int main()
                 window.close();
         }
 
+        /* Draw */
         window.clear();
+        window.draw(Status);
+        window.draw(s);
         for (int i = 0; i < fields * fields; i++)
         {
             window.draw(boardChess[i]);
