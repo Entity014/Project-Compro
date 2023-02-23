5e275c9ee467ee8d822c461153865e0def67af99
Holytis <holytis123@gmail.com>
2023-02-23 21:32:18 +0700

    Chess Move And Board by Tis's PC

--- Project/src/include/movement.cpp
+++ Project/src/include/movement.cpp
@@ -24,7 +24,7 @@ void movement(sf::Sprite sE[], sf::Vector2f bP[], sf::RectangleShape bC[], sf::V
                                 std::cout << isMove << " " << bC[0].getGlobalBounds().contains(mouse) << std::endl;
                         }
                 }
-        }// เขียนเหี้ยไรเนี้ย ควย
+        }// เขียนเหี้ยไรเนี้ย ควยๆ
         //  if(event sf::Event::MouseButtonReleased(sf::Mouse::Left))
         //         {
         //                 sf::FloatRect bounds = sE[0].getGlobalBounds();
--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -184,3 +184,4 @@ int main()
 
     return 0;
 }
+//test
